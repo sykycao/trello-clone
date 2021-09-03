a31ae4d66db4764876bbc51e80797497df9904a0
sykycao <sykycao@gmail.com>
2021-09-03 17:43:35 +0700

    submit on Enter press

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -9,12 +9,19 @@ type NewItemFormProps = {
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState('');
   const inputRef = useFocus();
+  const handleAddText = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      onAdd(text);
+    }
+  };
+
   return (
     <NewItemFormContainer>
       <NewItemInput
         ref={inputRef}
         value={text}
         onChange={(e) => setText(e.target.value)}
+        onKeyPress={handleAddText}
       />
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>
